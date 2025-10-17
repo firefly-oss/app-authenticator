7edffbee0b7de173065e07a9d0ecc73a252a9339
Carlos Sánchez <casc84ab@gmail.com>
2025-10-17 13:28:49 +0200

    Initialize app-orchestrator project with core, interfaces, and web modules.

---
+++ app-orchestrator-web/src/main/java/com/firefly/app/orchestrator/web/AppOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.firefly.app.orchestrator.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppOrchestratorApplication.class, args);
+    }
+
+}
