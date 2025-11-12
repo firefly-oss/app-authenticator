0b39d84e7c9edd0d7a989b6e47c7b7661603af2b
Carlos Sánchez <casc84ab@gmail.com>
2025-11-12 11:26:05 +0100

    Update @domainpassthrough annotations

--- app-authenticator-web/src/main/java/com/firefly/app/orchestrator/web/controllers/AuthenticatorController.java
+++ app-authenticator-web/src/main/java/com/firefly/app/orchestrator/web/controllers/AuthenticatorController.java
@@ -1,14 +1,15 @@
 package com.firefly.app.orchestrator.web.controllers;
 
 import com.firefly.common.application.config.DomainPassthrough;
+import com.firefly.common.application.config.DomainPassthroughs;
 import org.springframework.stereotype.Component;
 
 @Component
+@DomainPassthroughs({
+        @DomainPassthrough(path = "/api/v1/auth/login", target = "http://localhost:8085"),
+        @DomainPassthrough(path = "/api/v1/auth/logout", target = "http://localhost:8085"),
+        @DomainPassthrough(path = "/api/v1/users", target = "http://localhost:8085")
+})
 public class AuthenticatorController {
 
-    @DomainPassthrough(path = "/api/v1/auth/login", target = "http://localhost:8085")
-    public void login(){}
-
-    @DomainPassthrough(path = "/api/v1/auth/logout", target = "http://localhost:8085")
-    public void logout(){}
 }
