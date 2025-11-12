381289899341911f581f9c5d63f1fd554367d0f0
Carlos Sánchez <casc84ab@gmail.com>
2025-11-12 13:22:09 +0100

    Refactor app-orchestrator to app-authenticator: update class names, dependencies, configurations, and @DomainPassthrough targets.

--- app-authenticator-web/src/main/java/com/firefly/app/orchestrator/web/controllers/AuthenticatorController.java
+++ app-authenticator-web/src/main/java/com/firefly/app/orchestrator/web/controllers/AuthenticatorController.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 @DomainPassthroughs({
-        @DomainPassthrough(path = "/api/v1/auth/login", target = "http://localhost:8085"),
-        @DomainPassthrough(path = "/api/v1/auth/logout", target = "http://localhost:8085"),
-        @DomainPassthrough(path = "/api/v1/users", target = "http://localhost:8085")
+        @DomainPassthrough(path = "/api/v1/auth/login", target = "${endpoints.domain.security-center}"),
+        @DomainPassthrough(path = "/api/v1/auth/logout", target = "${endpoints.domain.security-center}"),
+        @DomainPassthrough(path = "/api/v1/users", target = "${endpoints.domain.security-center}")
 })
 public class AuthenticatorController {
 
